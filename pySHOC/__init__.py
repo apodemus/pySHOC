993a8d84799051114008cbcbcba677c5d7c7dc28
astromancer <hannes@saao.ac.za>
2020-01-08 13:03:39 +0200

    package refactor

--- pySHOC/__init__.py
+++ pySHOC/__init__.py
@@ -3,7 +3,7 @@ pySHOC - Data analysis tools for the Sutherland High-Speed Optical Cameras
 """
 
 from astropy.io.fits.hdu.base import register_hdu
-from recipes.iter import flatiter, itersubclasses
+# from recipes.iter import flatiter, itersubclasses
 
 from .core import *  # FIXME: THIS IS BAD!!!
 
@@ -15,32 +15,32 @@ register_hdu(shocNewHDU)
 register_hdu(shocOldHDU)
 
 
-# Collect named cubes in a dict
-namedObsClasses = {cls.kind: cls
-                   for cls in flatiter((itersubclasses(shocObs), shocObs))}
-namedRunClasses = {cls.obsClass.kind: cls
-                   for cls in flatiter((itersubclasses(shocRun), shocRun))}
-
-
-def cubeFactory(kind):  # not really a factory
-    return namedObsClasses.get(kind, shocObs)
-
-
-def runFactory(kind):
-    return namedRunClasses.get(kind, shocRun)
-
-
-def load(filenames, kind='science'):
-    """
-
-    Parameters
-    ----------
-    filenames
-    kind
-
-    Returns
-    -------
-
-    """
-    run = shocRun.load(filenames=filenames, kind=kind)
-    return run
+# # Collect named cubes in a dict
+# namedObsClasses = {cls.kind: cls
+#                    for cls in flatiter((itersubclasses(shocObs), shocObs))}
+# namedRunClasses = {cls.obsClass.kind: cls
+#                    for cls in flatiter((itersubclasses(shocRun), shocRun))}
+#
+#
+# def cubeFactory(kind):  # not really a factory
+#     return namedObsClasses.get(kind, shocObs)
+#
+#
+# def runFactory(kind):
+#     return namedRunClasses.get(kind, shocRun)
+#
+#
+# def load(filenames, kind='science'):
+#     """
+#
+#     Parameters
+#     ----------
+#     filenames
+#     kind
+#
+#     Returns
+#     -------
+#
+#     """
+#     run = shocRun.load(filenames=filenames, kind=kind)
+#     return run
