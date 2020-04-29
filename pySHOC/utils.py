87e2e68e9568a2e53befcd28d34fab02c81087fa
astromancer <hannes@saao.ac.za>
2020-04-29 11:23:49 +0200

    change caching location, optimized structure

--- pySHOC/utils.py
+++ pySHOC/utils.py
@@ -1,4 +1,3 @@
-import inspect
 import logging
 from pathlib import Path
 
@@ -6,8 +5,6 @@ import astropy.io.fits as pyfits
 from astropy.coordinates import SkyCoord
 from astropy.coordinates.name_resolve import NameResolveError
 
-from obstools import jparser
-# from decor.misc import persistent_memoizer
 from recipes.decor import memoize
 # from motley.profiling.timers import timer
 
@@ -17,13 +14,66 @@ from recipes.introspection.utils import get_module_name
 logger = logging.getLogger(get_module_name(__file__))
 
 # get coordinate cache file
-here = inspect.getfile(inspect.currentframe())  # this_filename
-moduleDir = Path(here).parent
-cooCacheName = '.cache/coords'
-cooCachePath = moduleDir / cooCacheName
+cachePath = Path.home() / '.cache/pySHOC'  # todo windows / mac ??
+cooCachePath = cachePath / 'coords.pkl'
+dssCachePath = cachePath / 'dss.pkl'
 
-dssCacheName = '.cache/dss'
-dssCachePath = moduleDir / dssCacheName
+
+class STScIServerError(Exception):
+    pass
+
+
+def get_coordinates(name_or_coords):
+    """
+    Get object coordinates from object name or string of coordinates. If the
+    coordinates could not be resolved, return None
+
+
+    Parameters
+    ----------
+    name_or_coords
+
+    Returns
+    -------
+    astropy.coordinates.SkyCoord or None
+    """
+    if isinstance(name_or_coords, SkyCoord):
+        return name_or_coords
+
+    try:
+        # first try interpret coords.
+        # eg. ImageRegistrationDSS('06:14:51.7 -27:25:35.5', (3, 3))
+        return SkyCoord(name_or_coords, unit=('h', 'deg'))
+    except ValueError:
+        return get_coords_named(name_or_coords)
+
+
+def get_coords_named(name):
+    """
+    Attempts to retrieve coordinates from name, first by parsing the name, or by
+    doing SIMBAD Sesame query for the coordinates associated with name.
+
+    Examples
+    --------
+    coords = get_coords_named('MASTER J061451.7-272535.5')
+    coords = get_coords_named('UZ For')
+    ...
+    """
+    try:
+        coo = resolver(name)
+    except NameResolveError as e:  # AttributeError
+        logger.warning(
+                'Coordinates for object %r could not be retrieved due to the '
+                'following exception: \n%s', name, str(e))
+    else:
+        if isinstance(coo, SkyCoord):
+            fmt = dict(precision=2, sep=' ', pad=1)
+            logger.info(
+                    'The following ICRS J2000.0 coordinates were retrieved:\n'
+                    'α = %s; δ = %s',
+                    coo.ra.to_string(unit='h', **fmt),
+                    coo.dec.to_string(unit='deg', alwayssign=1, **fmt))
+        return coo
 
 
 @memoize.to_file(cooCachePath)
@@ -33,50 +83,35 @@ def resolver(name):
     # faster than a sesame query
 
     # Attempts a SIMBAD Sesame query with the given object name
-    logging.info('Querying SIMBAD database for %r.', name)
-    return SkyCoord.from_name(name, parse=True)
+    logger.info('Querying SIMBAD database for %r.', name)
+    try:
+        return SkyCoord.from_name(name, parse=True)
+    except NameResolveError as e:
+        # check if the name is bad - something like "FLAT" or "BIAS", we want
+        # to cache these bad values also to avoid multiple sesame queries for
+        # bad values like these
+        if str(e).startswith("Unable to find coordinates for name"):
+            return None
+
+        # If we are here, it probably means there is something wrong with the
+        # connection:
+        # NameResolveError: "All Sesame queries failed."
+        raise
 
 
-def convert_skycoords(ra, dec, verbose=False):
+def convert_skycoords(ra, dec):
     """Try convert ra dec to SkyCoord"""
     if ra and dec:
         try:
             return SkyCoord(ra=ra, dec=dec, unit=('h', 'deg'))
-        except ValueError as err:
-            if verbose:
-                warn('Could not interpret coordinates: %s; %s' % (ra, dec))
-
-
-def retrieve_coords(name):
-    """
-    Attempts to retrieve coordinates from name, first by parsing the name, or by
-    doing SIMBAD Sesame query for the coordinates associated with name
-
-    Examples
-    --------
-    coords = retrieve_coords('MASTER J061451.7-272535.5')
-    coords = retrieve_coords('UZ For')
-    ...
-    """
-    try:
-        coo = resolver(name)
-        fmt = dict(precision=2, sep=' ', pad=1)
-        ra = coo.ra.to_string(unit='h', **fmt)
-        dec = coo.dec.to_string(unit='deg', alwayssign=1, **fmt)
-        logging.info(
-                'The following ICRS J2000.0 coordinates were retrieved:\n'
-                'RA = %s, DEC = %s', ra, dec)
-        return coo
-
-    except (NameResolveError, AttributeError) as e:
-        logging.warning(
-                'Coordinates for object %r could not be retrieved due to the '
-                'following exception: \n%s', name, str(e))
+        except ValueError:
+            logger.warning(
+                    'Could not interpret coordinates: %s; %s' % (ra, dec))
 
 
 def retrieve_coords_ra_dec(name, verbose=True, **fmt):
     """return SkyCoords and str rep for ra and dec"""
-    coords = retrieve_coords(name, verbose)
+    coords = get_coords_named(name)
     if coords is None:
         return None, None, None
 
@@ -106,19 +141,15 @@ def combine_single_images(ims, func):  # TODO MERGE WITH shocObs.combine????
     return pyfits.PrimaryHDU(data, header)  # outname
 
 
-class STScIServerError(Exception):
-    pass
-
-
 # @timer
-@memoize.to_file(dssCachePath)
-def get_dss(imserver, ra, dec, size=(10, 10), epoch=2000):
+@memoize.to_file(dssCachePath)  # memoize for performance
+def get_dss(server, ra, dec, size=(10, 10), epoch=2000):
     """
-    Grab a image from STScI server and load as HDUList
+    Grab a image from STScI server and load as HDUList.
 
     Parameters
     ----------
-    imserver
+    server
     ra
     dec
     size:   Field of view size in 'arcmin'
@@ -137,27 +168,26 @@ def get_dss(imserver, ra, dec, size=(10, 10), epoch=2000):
                      'poss2ukstu_red', 'poss1_red',
                      'poss2ukstu_ir',
                      'all')
-    if not imserver in known_servers:
+    if not server in known_servers:
         raise ValueError('Unknown server: %s.  Please select from: %s'
-                         % (imserver, str(known_servers)))
+                         % (server, str(known_servers)))
 
     # resolve size
     h, w = size  # FIXME: if number
 
     # make url
-    url = textwrap.dedent('''\
+    url = textwrap.dedent(f'''\
             http://archive.stsci.edu/cgi-bin/dss_search?
-            v=%s&
-            r=%f&d=%f&
-            e=J%f&
-            h=%f&w=%f&
+            v={server}&
+            r={ra}&d={dec}&
+            e=J{epoch}&
+            h={h}&w={w}&
             f=fits&
-            c=none''' % (imserver, ra, dec, epoch, h, w)
-                          ).replace('\n', '')
+            c=none''').replace('\n', '')
     # log
-    logging.info("Retrieving %s'x %s' image for object at J%.1f coordinates "
-                 "RA = %.3f; DEC = %.3f from %r", h, w, epoch, ra, dec,
-                 imserver)
+    logger.info("Retrieving %s'x %s' image for object at J%.1f coordinates "
+                "RA = %.3f; DEC = %.3f from %r", h, w, epoch, ra, dec, server)
+
     # get data from server
     data = urllib.request.urlopen(url).read()
     if b'ERROR' in data[:1000]:
@@ -169,11 +199,3 @@ def get_dss(imserver, ra, dec, size=(10, 10), epoch=2000):
     fitsData.write(data)
     fitsData.seek(0)
     return pyfits.open(fitsData, ignore_missing_end=True)
-
-
-
-
-import numpy as np
-from recipes.array import ndgrid
-# from obstools.fastfits import FitsCube
-#
