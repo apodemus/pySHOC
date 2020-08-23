535bbe890655ecad3ea61e547489300a42a771c7
astromancer <hannes@saao.ac.za>
2020-08-23 19:19:50 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
+#!/usr/bin/env bash
+
 ROOT=/home/hannes/work
 URLBASE = https://github.com/astromancer
 for name in recipes graphing motley obstools pySHOC; do
-    git clone $URLBASE/$name
-    python3 $SRC/$name/setup.py
+    git ls-remote ${name} -q
+    if [[ $? -eq 0 ]]; then
+        cd ${SRC}/${name}
+        git pull
+        cd ${SRC}
+    else
+        git clone ${URLBASE}/${name}
+        git checkout dev
+        python3 ${SRC}/${name}/setup.py
+    fi
     done
